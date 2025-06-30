5d61e58663ff9a5b2276cee323de7953f7a4ff29
hachoj <hjchojnowski@gmail.com>
2025-06-29 20:59:50 -0400

    basic MongoDB functionality
    Created a basic set of functions and templates for MongoDB testing so it will be easy to use and get started.
    Also created environment file.

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from app.models import test_insert, test_query
 
 def create_app():
     app = Flask(__name__)
 
+    app.config["MONGO_URI"] = "mongodb+srv://hachoj:yJsUEtLxpnqobesK@yafa-db.yvwbol0.mongodb.net/flask_app?retryWrites=true&w=majority&appName=yafa-db"
+    mongo = PyMongo(app)
+    app.mongo = mongo
+
     # from flask quickstart guide
     @app.route("/")
     def hello_world():
-        return "<p>Hello, World!</p>"
+        test_insert(app.mongo)
+        return render_template("test_greeting.html")
 
-    @app.route("/<name>")
-    def hello(name):
-        return render_template("template.html", name=name)
+    @app.route("/test_query")
+    def hello():
+        query_result =test_query(app.mongo)
+        return render_template("test_query.html", query_result=query_result)
 
     return app 
