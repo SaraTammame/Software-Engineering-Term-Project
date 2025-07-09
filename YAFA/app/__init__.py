2279137de551c2145c713fcba32c69e074af0d32
TricanMan <lew@ufl.edu>
2025-07-09 11:13:56 -0400

    Update readme, added basic home page, began sql-alchemy set up

--- YAFA/app/__init__.py
+++ YAFA/app/__init__.py
@@ -7,21 +7,42 @@ import os
 load_dotenv()
 
 db = SQLAlchemy()
-migrate = Migrate()
+# migrate = Migrate()
 
+def create_app(test_config=None):
 
-def create_app():
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
 
-    app = Flask(__name__)
+    # Configure your database URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+#     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+    # Initialize the extension with the app
     db.init_app(app)
-    migrate.init_app(app, db)
 
-    from app.routes import user_routes
+#     migrate.init_app(app, db)
 
-    app.register_blueprint(user_routes.user_bp)
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # basic home page
+    @app.route('/')
+    def home():
+        return render_template('home.html')
+
+#     from app.routes import user_routes
+
+#     app.register_blueprint(user_routes.user_bp)
 
     return app
